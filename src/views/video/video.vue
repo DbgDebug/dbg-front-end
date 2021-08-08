e42eda5228a57727927943f3544cb237ac9638c9
dbg <maxiaoha@live.com>
2021-08-08 16:56:39 +0800

    视频页面初始化

---
+++ src/views/video/video.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-container">
+    ...
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'Video'
+})
+export default class extends Vue {
+  created() {
+    //
+  }
+
+  mounted() {
+    //
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.edit-input {
+  padding-right: 100px;
+}
+
+.cancel-btn {
+  position: absolute;
+  right: 15px;
+  top: 10px;
+}
+</style>
